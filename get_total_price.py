c897b357ac17dd265b0df14366db93505191451e
fame <famephp@gmail.com>
2022-11-14 10:28:31 +0500

    04

--- get_total_price.py
+++ get_total_price.py
@@ -1,9 +1,20 @@
 def get_total_price(data:str)->float:
     """
     This function returns the total price of the fruits in the list
-
     args:
         data: CSV file with the fruits data
     returns:
         list of fruits total price
     """
+    data = data.split()[1:]
+    count = 0.0
+
+    for i in data:
+        val = float(i.split(",")[1])
+        print(val)
+        count += val
+        
+    print("{:.1f}".format(count))
+
+data = open('fruits.csv').read()
+print(get_total_price(data))
